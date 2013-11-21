9ef6ecccdff055f9023c7b723f55047102917e7b
Renato Zust <renatozust@gmail.com>
2013-11-20 20:01:17 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Occutec::Application.config.secret_key_base = 'f11f40b46598f791ee9c5c6c15062f7024721f0d197a1f829b64155764ad30c76e63543246c7d75ece9902fbc7fd84a6e2c351191e23e995e3229f929853bdbd'
